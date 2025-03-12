6358df16196e31de62c2b00a03966e82dc72be0e
Arthur Martins <arthurhpm0519@gmail.com>
2025-03-12 16:13:59 -0300

    backend maap

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ new DatabaseModel().testeConexao().then((resdb) => {
     if(resdb){
         console.log('Conexão com banco de dados realizada com sucesso.');
         // iniciando o servidor
+        
         server.listen(port, ()  => {
             console.log(`Servidor iniciado no endereço http://localhost:${port}`);
         });
