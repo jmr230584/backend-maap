e40d6bc3d968cf5e252df5a0fbabb969c8d9871b
Pedro Camolesi <potaviocamolesi@gmail.com>
2025-06-06 09:08:53 -0300

    .

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,23 +1,25 @@
 const SERVER_ROUTES = {
     LISTAR_MEDICOS: '/listar/medicos',
     NOVO_MEDICO: '/cadastro/medico',
-    REMOVER_MEDICO: '/remover/medico',
-    ATUALIZAR_MEDICO: '/atualizar/medico',
+    REMOVER_MEDICO: '/remover/medico/:idMedico',
+    ATUALIZAR_MEDICO: '/atualizar/medico/:idMedico',
  
     LISTAR_PACIENTES: '/listar/pacientes',
     NOVO_PACIENTE: '/cadastro/paciente',
-    REMOVER_PACIENTE: '/remover/paciente',
-    ATUALIZAR_PACIENTE: '/atualizar/paciente',
+    REMOVER_PACIENTE: '/remover/paciente/:idPaciente',
+    ATUALIZAR_PACIENTE: '/atualizar/paciente/:idPaciente',
 
     LISTAR_CONSULTAS: '/listar/consultas',
     NOVA_CONSULTA: '/cadastro/consulta',
-    REMOVER_CONSULTA: '/remover/consulta',
-    ATUALIZAR_CONSULTA: '/atualizar/consulta',
+    REMOVER_CONSULTA: '/remover/consulta/:idConsulta',
+    ATUALIZAR_CONSULTA: '/atualizar/consulta/:idConsulta',
 
-    NOVO_USUARIO: '/cadastro/usuario',
-    ATUALIZAR_USUARIO: '/atualizar/usuario',
-    REMOVER_USUARIO: '/remover/usuario',
     LISTAR_USUARIOS: '/listar/usuarios',
+    NOVO_USUARIO: '/cadastro/usuario',
+    REMOVER_USUARIO: '/remover/usuario/:idUsuario',
+    ATUALIZAR_USUARIO: '/atualizar/usuario/:idUsuario',
+    
+   
     
 }
 
